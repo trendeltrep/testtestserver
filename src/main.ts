be3a55c84154f1fc9665e1d07aa900d73e2f8f4c
trendeltrep <maxim.krikukha20031@gmail.com>
2025-04-29 22:06:51 +0300

    2

--- src/main.ts
+++ src/main.ts
@@ -6,30 +6,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000',
+      'https://inquire-client-9ehhmjtet-maksyms-projects-e7f962b7.vercel.app/', 
+      'https://inquire-client-coral.vercel.app/'],
   });
-  
+
   app.setGlobalPrefix('api');
 
-  const postsService = app.get(PostsService);
-
-  const existingPosts = await postsService.findAll();
-  if (existingPosts.length === 0) {
-    await seedPosts(postsService);
-    console.log('Seeded 12 posts');
-  }
+ 
 
   await app.listen(3001);
 }
 bootstrap();
 
-async function seedPosts(postsService: PostsService) {
-  const seedData = Array.from({ length: 12 }).map((_, i) => ({
-    title: `Post ${i + 1}`,
-    content: `This is the content of post number ${i + 1}.`,
-  }));
-
-  for (const post of seedData) {
-    await postsService.create(post);
-  }
-}
