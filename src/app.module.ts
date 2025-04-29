fd8f627358af11d67683927af6f9791222577ef6
trendeltrep <maxim.krikukha20031@gmail.com>
2025-04-29 22:19:57 +0300

    3

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { Post } from './posts/post.entity';
 import { Comment } from './comments/comment.entity';
 import { PostsModule } from './posts/post.module';
@@ -7,15 +8,18 @@ import { CommentsModule } from './comments/comment.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, 
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '12345',
-      database: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT || '5432', 10),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [Post, Comment],
-      synchronize: true,
+      synchronize: true, 
     }),
     PostsModule,
     CommentsModule,
